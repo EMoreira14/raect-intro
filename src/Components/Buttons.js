4a93d7717f964fa5f5e22f2024872c923f3e13ea
NCONTED <NCONTED@mynorth.mymdc.net>
2020-02-04 11:38:29 -0500

    first commit

---
+++ src/Components/Buttons.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Button = () => {
+    return (
+        <div><button type="button" class="btn btn-primary"> Click Me
+        </button>
+          </div>
+    )
+}
+export default Button;
